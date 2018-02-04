86d2e5cd1167050fc93055ee1c85f69f138c273a
Michael Kang <michaelkang85@gmail.com>
2018-02-04 15:34:27 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,4 +1,12 @@
 # Write your code here.
 def badge_maker(name)
-  "Hello, my name is #{name}"
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(arr)
+  newArr = []
+  arr.each do |ele|
+    newArr << badge_maker(ele)
+  end
+  return newArr
 end
